bb11d440fdc71e891c53df1b1d155eb4d62b7080
oceanshen <oshen@udel.edu>
2022-12-02 09:39:20 -0500

    initial commit

---
+++ app/src/main/java/com/example/colorscanner/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.colorscanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // a comment
+    }
+}
